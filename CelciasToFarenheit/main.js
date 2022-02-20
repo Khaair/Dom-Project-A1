627428cbd9d15428ee5d5daf29605cd690162dd7
Khair <khair.cse.iu@gmail.com>
2022-02-20 17:36:30 +0600

    Update

--- CelciasToFarenheit/main.js
+++ CelciasToFarenheit/main.js
@@ -2,7 +2,7 @@ function convert(){
     var cel = document.getElementById('data').value;
     const factor = 9/5
     var far = (cel * factor) + 32
-    document.getElementById('res').innerText= far + " far";
+    document.getElementById('res').innerText=  cel +" ^Celcius"+ " = " + far + " Farenheit ";
 
    
 
