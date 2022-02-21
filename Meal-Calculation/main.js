932ee85447c87444a503425aab1501d273696662
Khair <khair.cse.iu@gmail.com>
2022-02-21 20:31:16 +0600

    Update

--- Meal-Calculation/main.js
+++ Meal-Calculation/main.js
@@ -1,12 +1,22 @@
-var myArr = [];
+var myArr1 = [];
 
+
+var myArr2 = [];
 
 function sum(a){
-var summ = myArr.reduce((a, b) => a + b, 0);
+var summ = myArr1.reduce((a, b) => a + b, 0);
 var rate = summ/a
 document.getElementById('show').innerHTML = summ;
 return rate
 }
+
+
+function sum(a){
+    var summ = myArr2.reduce((a, b) => a + b, 0);
+    var rate = summ/a
+    document.getElementById('show2').innerHTML = summ;
+    return rate
+    }
 
 
 function total(){
@@ -17,24 +27,49 @@ function total(){
 
 
 
-function  Calculation(){
+function  Calculation1(){
     
        var inputText = document.getElementById('data').value;
 
        let num = parseFloat(inputText)
        
-       myArr.push(num);
+       myArr1.push(num);
        
        var pval = "";
        
-       for(i = 0; i < myArr.length; i++)
+       for(i = 0; i < myArr1.length; i++)
        {
-           pval = pval + myArr[i]
+           pval = pval + myArr1[i]
        }
        
      
-       document.getElementById('res').innerHTML = myArr;
+       document.getElementById('res').innerHTML = myArr1;
 
    
+
+}
+
+
+
+
+function  Calculation2(){
+    
+    var inputText = document.getElementById('data2').value;
+
+    let num = parseFloat(inputText)
+    
+    myArr2.push(num);
+    
+    var pval = "";
+    
+    for(i = 0; i < myArr2.length; i++)
+    {
+        pval = pval + myArr2[i]
+    }
+    
+  
+    document.getElementById('res2').innerHTML = myArr2;
+
+
 
 }
